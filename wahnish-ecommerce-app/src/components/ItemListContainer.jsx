2fe001125d817357807a77215621f845f86d6b68
Emma Wahnish <ewahnish@gmail.com>
2022-02-28 19:46:25 -0300

    mensaje

--- wahnish-ecommerce-app/src/components/ItemListContainer.jsx
+++ wahnish-ecommerce-app/src/components/ItemListContainer.jsx
@@ -1,5 +1,6 @@
 import '../App.css';
 const ItemListContainer=( {saludo})=> {
+    saludo = "Bienvenidos!! Conozcan nuestros Platos Internacionales!!"
     return (
         <div class="flex-container">
             <h2>{saludo}</h2>
