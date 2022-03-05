e3a5880701bd808c5629e5e7850d19c6413adaf2
Emma Wahnish <ewahnish@gmail.com>
2022-03-05 20:44:16 -0300

    Styles

--- wahnish-ecommerce-app/src/Container/ItemListContainer.jsx
+++ wahnish-ecommerce-app/src/Container/ItemListContainer.jsx
@@ -7,27 +7,35 @@ import Greeting from '../components/Greeting.jsx';
 const ItemListContainer = () => {
     const saludo = "Bienvenidos!! Conozcan nuestros platos Internacionales!!"
     const [dishes, setDishes] = useState([{}]);
+    const [bool, setBoolean] = useState(true);
+    console.log(bool)
 
     useEffect(() => {
         getFetch()
-            .then((response) => setDishes(()=>response))
+            .then((response) => setDishes(() => response))
             .catch((err) => console.log(err))
-            .finally("final")
+            .finally(() => setBoolean(() => false))
     }, []);
 
-    console.log("dishes ", dishes);
-
-    return (
-        <div>
-            <div className="flex-container">
-                <Greeting saludo={saludo} />
+    if (bool) {
+        return (
+            <div className="spinner">
+                <div id="circle">
+                    <i className="fas fa-spinner"></i>
+                </div>
+            </div>)
+    } else {
+        return (
+            <div>
+                <div className="flex-container">
+                    <Greeting saludo={saludo} />
+                </div>
+                <div className="flex-container-card py-10">
+                    <ItemList item={dishes} />
+                </div>
             </div>
-
-            <div className="flex-container-card py-10">
-                <ItemList item = {dishes} />
-            </div>
-        </div>
-    );
+        );
+    }
 
 }
 
