3ba93278f7ec80416e4601df2fcb16751f3753c2
Emma Wahnish <ewahnish@gmail.com>
2022-04-06 09:17:58 -0300

    Bug paginacion arreglado

--- wahnish-ecommerce-app/src/components/ItemList.jsx
+++ wahnish-ecommerce-app/src/components/ItemList.jsx
@@ -1,20 +1,25 @@
 import '../App.css';
 import { memo } from "react";
+import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import ReactPaginate from 'react-paginate';
 import Item from './Item.jsx';
 const ItemList = ({ item }) => {
     const [pageNumber, setPageNumber] = useState(0);
+
     const changePage = ({ selected }) => {
         setPageNumber(selected);
     }
+
     const itemsPerPage = 3;
     const pagesVisited = pageNumber * itemsPerPage;
     const pageCount = Math.ceil(item.length / itemsPerPage)
 
+    const thePath = useParams()
+
     useEffect(() => {
         setPageNumber(0);
-      }, []);
+      }, [thePath]);
     
 
     const displayItems =
